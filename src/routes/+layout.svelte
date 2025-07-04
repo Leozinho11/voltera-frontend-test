9b4e48c21c993f0148d3d34213396c80a1bed46f
Leo Barreto <leoferraribarreto@hotmail.com>
2025-07-04 04:36:39 -0300

    Update title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,7 @@
 <svelte:head>
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<title>Sistema de Estimativa de Idade</title>
 	<link
 		href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
 		rel="stylesheet"
